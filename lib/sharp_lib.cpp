32f245f3349fedb4fab395cec86f760c914ab622
Andrei9383 <andreistan275@gmail.com>
2023-01-27 23:03:40 +0200

    Create coloring and line functions

--- lib/sharp_lib.cpp
+++ lib/sharp_lib.cpp
@@ -1,20 +1,20 @@
 #include <vector>
 
 namespace sharp {
-  typedef struct {
+  struct Pixel{
     int R = 0;
     int G = 0;
     int B = 0;
-  }Pixel;
+  };
 
   using canvas = std::vector<std::vector<Pixel>>;
 
-  canvas Canvas(const int p_Width, const int p_Height){
+  auto Canvas(const int p_Width, const int p_Height) -> canvas{
     canvas load(p_Height, std::vector<Pixel>(p_Width));
     return load;
   };
 
-  void PrintCanvas(canvas p_Canvas){
+  auto PrintCanvas(canvas p_Canvas) -> void{
     for(int i = 0; i<p_Canvas.size(); i++){
       for(int j = 0; j<p_Canvas[i].size(); j++){
         std::cout << "(" << p_Canvas[i][j].R << "," 
@@ -24,6 +24,21 @@ namespace sharp {
       std::cout << std::endl;
     }
   };
-  
+
+  namespace render{
+    auto color(canvas &p_Canvas, std::pair<int, int> p_Point, Pixel p_Color) -> void{
+      p_Canvas[p_Point.first][p_Point.second].R = p_Color.R;
+      p_Canvas[p_Point.first][p_Point.second].G = p_Color.G;
+      p_Canvas[p_Point.first][p_Point.second].B = p_Color.B;
+    }
+    auto line(canvas &p_Canvas, std::pair<int, int> p_Point1, std::pair<int, int> p_Point2, Pixel p_Color) -> void{
+      int dx = p_Point2.first - p_Point1.first;
+      int dy = p_Point2.second - p_Point2.second;
+      for(int x = p_Point1.first; x<p_Point2.first; x++){
+        int y = p_Point1.second + dy + (x - p_Point1.first) / dx;
+        color(p_Canvas, std::make_pair(x, y), p_Color);
+      }
+    }
+  }
   
 }
--- sharp.cpp
+++ sharp.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <utility>
 #include "../lib/sharp_lib.cpp"
 
-const int WIDTH = 100;
-const int HEIGHT = 100;
+const int WIDTH = 10;
+const int HEIGHT = 10;
 
-int main(void) {
-  auto c = sharp::Canvas(WIDTH, HEIGHT);
+auto main(void) -> int {
+  sharp::canvas c = sharp::Canvas(WIDTH, HEIGHT);
+  sharp::Pixel pix = {10, 10, 10};
+  sharp::render::line(c, std::make_pair(1, 1), std::make_pair(4, 4), pix);
+  sharp::render::color(c, std::make_pair(2, 2), pix);
   sharp::PrintCanvas(c);
   return 0;
 }
